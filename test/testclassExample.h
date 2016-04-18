85d71ef4264d834d24f093ca60343a04d84ae080
Ethan Slattery <TheSlat@users.noreply.github.com>
2016-04-17 17:49:40 -0700

    Update testclassExample.h
    improved comments in example test case

--- test/testclassExample.h
+++ test/testclassExample.h
@@ -8,24 +8,24 @@ class TestingSuiteOne : public QObject
 {
  Q_OBJECT
 private slots:
-    /*** GENERAL TEST SETUP AND TEARDOWN ***/
-    // Called before every test, if it fails then the NEXT test does not run
-    void initTestCase() { qDebug("initTestCase() is called before each test!"); }
-    // Called after the last test function was executed.
-    void cleanupTestCase() {}
-
-    /*** PER-TEST SETUP AND TEARDOWN ***/
+    /*** GENERAL TEST SETUP AND TEARDOWN - Comment out if not defined ***/
     // Called before the first test function is executed. If this fails no tests are run
+    void initTestCase() { qDebug("initTestCase() is called before each test class!"); }
+    // Called after the last test function was executed.
+    void cleanupTestCase() { qDebug("cleanupTestCase() runs after all tests in this class are done!"); }
+
+    /*** PER-TEST SETUP AND TEARDOWN  - Comment out if not defined ***/
+    // Called before every test, if it fails then the NEXT test does not run
     void init() { qDebug("init() is called before every function!"); }
-    // Called after every test function
+    // Called after every test function finishes
     void cleanup() { qDebug("cleanup() runs after every test!"); }
 
     /*** TESTS FOR THE CLASS ***/
-    //void initTest();
     void testingTest() { qDebug("*** RUNNING THE ACTUAL TEST ***"); QVERIFY(true); }
 
 };
 
+/*** Uncomment this line in a real test class to add to list of tests ***/
 //DECLARE_TEST(TestingSuiteOne)
 
 #endif // TESTCLASS_H
